692b8c4b4d13f0c7e3cb7856b35c4e9afd4a672e
noobmaster1112 <noobmaster1112@outlook.com>
2022-01-11 05:44:11 -0500

    API related error check

--- scan.py
+++ scan.py
@@ -5,4 +5,20 @@ import config
 
 URL = "https://www.virustotal.com/api/v3/"
 
+# Checks for API related errors
+def api_check(resp):
+    if r.status_code == 401:
+        print("ERROR: Check your API key")
+        return False
+    elif r.status_code == 429:
+        print("ERROR: Quota exeeded")
+        return False
+    elif r.status_code in [400,403,404,409,424,503,504]:
+        print("ERROR: Something went wrong")
+        return False
+    else:
+        return True
 
+
+if __name__ == "__main__":
+    api_check()
